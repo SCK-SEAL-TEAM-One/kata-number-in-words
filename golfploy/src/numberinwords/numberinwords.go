1b2caef4b322ee5aa8b9d5ce7f4a93913e348f0e
Apipol Sukgler <apipol@computerlogy.com>
2018-09-07 10:32:32 +0700

    [Edited]: refactor ให้ไม่ hard code และเพิ่ม test Test_ConvertDecimalToThaiCharactor_Input_400000_Should_Be_Four_Hundred_Thousand_Baht และ Test_ConvertDecimalToThaiCharactor_Input_50000_Should_Be_Fifty_Thousand_Baht

--- golfploy/src/numberinwords/numberinwords.go
+++ golfploy/src/numberinwords/numberinwords.go
@@ -6,13 +6,34 @@ import (
 
 func ConvertDecimalToThaiCharactor(amountMoney float64) string {
 	stringAmountMoney := fmt.Sprintf("%d", int(amountMoney))
+	unitMap := map[int]string{
+		6: "แสน",
+		5: "หมื่น",
+		4: "พัน",
+		3: "ร้อย",
+		2: "สิบ",
+		1: "",
+	}
+	thaiCharactor := map[string]string{
+		"1": "หนึ่ง",
+		"2": "สอง",
+		"3": "สาม",
+		"4": "สี่",
+		"5": "ห้า",
+		"6": "หก",
+		"7": "เจ็ด",
+		"8": "แปด",
+		"9": "เก้า",
+		"0": "",
+	}
 	unit := len(stringAmountMoney)
+	var thaiNumber string
 	for _, runeValue := range stringAmountMoney {
-		if unit == 6 && string(runeValue) == "1" {
-			thaiNumber := "หนึ่งแสน"
-			return thaiNumber + "บาทถ้วน"
+		if string(runeValue) != "0" {
+			thaiNumber += fmt.Sprintf("%s%s", thaiCharactor[string(runeValue)], unitMap[unit])
 		}
+		unit--
 	}
 
-	return ""
+	return thaiNumber + "บาทถ้วน"
 }
--- golfploy/src/numberinwords/numberinwords_test.go
+++ golfploy/src/numberinwords/numberinwords_test.go
@@ -15,3 +15,25 @@ func Test_ConvertDecimalToThaiCharactor_Input_100000_Should_Be_One_Hundred_Thous
 		t.Errorf("expect %s but it got %s", expectedResult, actualResult)
 	}
 }
+
+func Test_ConvertDecimalToThaiCharactor_Input_400000_Should_Be_Four_Hundred_Thousand_Baht(t *testing.T) {
+	expectedResult := "สี่แสนบาทถ้วน"
+	amountMoney := 400000.00
+
+	actualResult := ConvertDecimalToThaiCharactor(amountMoney)
+
+	if expectedResult != actualResult {
+		t.Errorf("expect %s but it got %s", expectedResult, actualResult)
+	}
+}
+
+func Test_ConvertDecimalToThaiCharactor_Input_50000_Should_Be_Fifty_Thousand_Baht(t *testing.T) {
+	expectedResult := "ห้าหมื่นบาทถ้วน"
+	amountMoney := 50000.00
+
+	actualResult := ConvertDecimalToThaiCharactor(amountMoney)
+
+	if expectedResult != actualResult {
+		t.Errorf("expect %s but it got %s", expectedResult, actualResult)
+	}
+}
