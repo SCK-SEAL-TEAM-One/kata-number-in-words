0969e332495257568382cfa62cc122a28e076817
Apipol Sukgler <apipol@computerlogy.com>
2018-09-07 10:18:10 +0700

    [Edited]:ปรับโค้ดให้สามารถทำงานได้ด้วยการคำนวณไม่hard code

--- golfploy/src/numberinwords/numberinwords.go
+++ golfploy/src/numberinwords/numberinwords.go
@@ -1,6 +1,18 @@
 package numberinwords
 
-func ConvertDecimalToThaiCharactor(amountMoney float64) string {
+import (
+	"fmt"
+)
 
-	return "หนึ่งแสนบาทถ้วน"
+func ConvertDecimalToThaiCharactor(amountMoney float64) string {
+	stringAmountMoney := fmt.Sprintf("%d", int(amountMoney))
+	unit := len(stringAmountMoney)
+	for _, runeValue := range stringAmountMoney {
+		if unit == 6 && string(runeValue) == "1" {
+			thaiNumber := "หนึ่งแสน"
+			return thaiNumber + "บาทถ้วน"
+		}
+	}
+
+	return ""
 }
