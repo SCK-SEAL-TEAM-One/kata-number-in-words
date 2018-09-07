9192c832cf21247aebfcc825a4eb5466c5b4fe15
platylm <n_nareenart@kkumail.com>
2018-09-07 09:52:01 +0700

    [Created]:function convertNumber

---
+++ puklek/src/numberconvert/convertNumber_test.go
@@ -0,0 +1,19 @@
+package numberconvert_test
+
+import (
+	. "numberconvert"
+	"testing"
+)
+
+func Test_ConvertNumber_Input_100000_Dot_00_Should_Be_One_Hundred_Thousand_Baht(t *testing.T) {
+	expected := "หนึ่งแสนบาทถ้วน"
+
+	money := 100000.00
+
+	actual := ConvertNumber(money)
+
+	if expected != actual {
+		t.Errorf("expected %s but got it %s", expected, actual)
+	}
+
+}
