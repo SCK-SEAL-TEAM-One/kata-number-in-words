b0fd161ab6d8d91daca627d50d04db9d12c9c769
smalldog124 <panumars124@gmail.com>
2018-09-09 00:52:59 +0700

    [Edited]:เพื่มเทส Input เป็น 500000 และ  แก้ไขฟังก์ชันให้ทำงานถูกต้อง

--- puklek/src/numberconvert/convertNumber.go
+++ puklek/src/numberconvert/convertNumber.go
@@ -1,6 +1,43 @@
 package numberconvert
 
-func ConvertNumber(money float64) string {
+import (
+	"fmt"
+)
 
-	return "หนึ่งแสนบาทถ้วน"
+func ConvertNumber(money float64) string {
+	unitNumber := map[int]string{
+		1: "",
+		2: "สิบ",
+		3: "ร้อย",
+		4: "พัน",
+		5: "หมื่น",
+		6: "แสน",
+	}
+	number := map[string]string{
+		"1": "หนึ่ง",
+		"2": "สอง",
+		"3": "สาม",
+		"4": "สี่",
+		"5": "ห้า",
+		"6": "หก",
+		"7": "เจ็ด",
+		"8": "แปด",
+		"9": "เก้า",
+		"0": "",
+	}
+
+	var convertNumber string
+
+	moneyString := fmt.Sprintf("%d", int(money))
+
+	units := len(moneyString)
+
+	for _, unitMoney := range moneyString {
+		if string(unitMoney) != "0" {
+			convertNumber += fmt.Sprintf("%s%s", number[string(unitMoney)], unitNumber[units])
+			units--
+		}
+	}
+
+	return convertNumber + "บาทถ้วน"
 }
--- puklek/src/numberconvert/convertNumber_test.go
+++ puklek/src/numberconvert/convertNumber_test.go
@@ -17,3 +17,15 @@ func Test_ConvertNumber_Input_100000_Dot_00_Should_Be_One_Hundred_Thousand_Baht(
 	}
 
 }
+func Test_ConvertNumber_Input_500000_Dot_00_Should_Be_Five_Hundred_Thousand_Baht(t *testing.T) {
+	expected := "ห้าแสนบาทถ้วน"
+
+	money := 500000.00
+
+	actual := ConvertNumber(money)
+
+	if expected != actual {
+		t.Errorf("expected %s but got it %s", expected, actual)
+	}
+
+}
