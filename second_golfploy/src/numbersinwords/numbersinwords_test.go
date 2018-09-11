e91585ee7d975f34a9cef78810505c161b75067a
ployploy <chonnikan.t@kkumail.com>
2018-09-11 10:06:28 +0700

    [Added]:Test_ConvertNumberToThaiCharactor_Input_50000_Should_Be_Fifty_Thousand_Baht เพื่อทดสอบการทำงานขชอง ConvertNumberToThaiCharactor

--- second_golfploy/src/numbersinwords/numbersinwords_test.go
+++ second_golfploy/src/numbersinwords/numbersinwords_test.go
@@ -26,3 +26,14 @@ func Test_ConvertNumberToThaiCharactor_Input_400000_Should_Be_Four_Hundred_Thous
 		t.Errorf("expect %s but it got %s", expectedResult, actualResult)
 	}
 }
+
+func Test_ConvertNumberToThaiCharactor_Input_50000_Should_Be_Fifty_Thousand_Baht(t *testing.T) {
+	expectedResult := "ห้าหมื่นบาทถ้วน"
+	inputNumber := 50000.00
+
+	actualResult := ConvertNumberToThaiCharactor(inputNumber)
+
+	if expectedResult != actualResult {
+		t.Errorf("expect %s but it got %s", expectedResult, actualResult)
+	}
+}
