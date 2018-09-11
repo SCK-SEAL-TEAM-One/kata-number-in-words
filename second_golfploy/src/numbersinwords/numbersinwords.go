82201d3a4745408a841fdd8c1b3c65826c495437
ployploy <chonnikan.t@kkumail.com>
2018-09-11 10:04:30 +0700

    [Added]:Test_ConvertNumberToThaiCharactor_Input_400000_Should_Be_Four_Hundred_Thousand_Baht เพิ่มทดสอบการทำงานฟังก์ชั่น ConvertNumberToThaiCharactor

--- second_golfploy/src/numbersinwords/numbersinwords.go
+++ second_golfploy/src/numbersinwords/numbersinwords.go
@@ -3,18 +3,35 @@ package numbersinwords
 import "strconv"
 
 func ConvertNumberToThaiCharactor(inputNumber float64) string {
+	var output string
+	numberThai := map[string]string{
+		"1": "หนึ่ง",
+		"2": "สอง",
+		"3": "สาม",
+		"4": "สี่",
+		"5": "ห้า",
+		"6": "หก",
+		"7": "เจ็ด",
+		"8": "แปด",
+		"9": "เก้า",
+		"0": "",
+	}
+	unitThai := map[int]string{
+		6: "แสน",
+		5: "หมื่น",
+		4: "พัน",
+		3: "ร้อย",
+		2: "สิบ",
+		1: "",
+	}
 	number := int(inputNumber)
 	stringNumber := strconv.Itoa(number)
 	unit := len(stringNumber)
-	var numberThai string
-	var unitThai string
 	for _, numberCharactor := range stringNumber {
-		if string(numberCharactor) == "1" {
-			numberThai = "หนึ่ง"
-			if unit == 6 {
-				unitThai = "แสนบาทถ้วน"
-			}
+		if string(numberCharactor) != "0" {
+			output += numberThai[string(numberCharactor)] + unitThai[unit]
 		}
+		unit--
 	}
-	return numberThai + unitThai
+	return output + "บาทถ้วน"
 }
