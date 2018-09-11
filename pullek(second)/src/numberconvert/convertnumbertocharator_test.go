9891530f675754e022fcb3273b8e084de39ad943
smalldog124 <panumars124@gmail.com>
2018-09-11 09:53:26 +0700

    [Added]:เพิ่มเทสที่ input 50000

--- pullek(second)/src/numberconvert/convertnumbertocharator_test.go
+++ pullek(second)/src/numberconvert/convertnumbertocharator_test.go
@@ -25,3 +25,13 @@ func Test_NumberConvert_Input_500000_Should_Be_Five_Hundred_Thousand_Baht(t *tes
 		t.Errorf("expect %s but it got %s", expectedNumber, actual)
 	}
 }
+func Test_NumberConvert_Input_50000_Should_Be_Fifty_Thousand_Baht(t *testing.T) {
+	expectedNumber := "ห้าหมื่นบาทถ้วน"
+	number := 50000
+
+	actual := numberconvert.NumberConvert(number)
+
+	if expectedNumber != actual {
+		t.Errorf("expect %s but it got %s", expectedNumber, actual)
+	}
+}
