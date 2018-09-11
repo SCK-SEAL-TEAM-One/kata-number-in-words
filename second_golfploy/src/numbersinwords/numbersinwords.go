3dee8ebcd145bcea92425e368ddb0310795a7210
ployploy <chonnikan.t@kkumail.com>
2018-09-11 09:51:26 +0700

    [Edited]: เขียนฟังก์ชั่น ConvertNumberToThaiCharactor โดยแก้จากส่งค่าออกตรงๆเป็นการใส่วิธีคิดเข้าไปแทน

--- second_golfploy/src/numbersinwords/numbersinwords.go
+++ second_golfploy/src/numbersinwords/numbersinwords.go
@@ -1,5 +1,20 @@
 package numbersinwords
 
+import "strconv"
+
 func ConvertNumberToThaiCharactor(inputNumber float64) string {
-	return "หนึ่งแสนบาทถ้วน"
+	number := int(inputNumber)
+	stringNumber := strconv.Itoa(number)
+	unit := len(stringNumber)
+	var numberThai string
+	var unitThai string
+	for _, numberCharactor := range stringNumber {
+		if string(numberCharactor) == "1" {
+			numberThai = "หนึ่ง"
+			if unit == 6 {
+				unitThai = "แสนบาทถ้วน"
+			}
+		}
+	}
+	return numberThai + unitThai
 }
